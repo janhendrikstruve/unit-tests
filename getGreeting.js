2c7183fc285a997adbde2e0205a12af52757a0ec
janhendrikstruve <janhendrikstruve@gmail.com>
2021-10-18 11:56:51 +0200

    Add nodemon and gitignore

---
+++ getGreeting.js
@@ -0,0 +1,5 @@
+export function getGreeting(name = 'you') {
+  return `Hello ${name}!` // template literal
+
+  // return 'Hello ' + name + '!'
+}
--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-function getGreeting(name = 'you') {
-  return `Hello ${name}!` // template literal
-  // return 'Hello ' + name + '!'
-}
+import { getGreeting } from './getGreeting.js'
+import { replaceName } from './replaceName.js'
 
 console.log(getGreeting('Jane Doe'))
-
-function replaceName(text, name, newName) {
-  return text.replaceAll(name, newName)
-}
 
 console.log(replaceName('hello Jane', 'Jane', 'John'))
 console.log(replaceName('Hello Jane, Goodbye Jane', 'Jane', 'John'))
